c2ce393061c1b58301702ed1911d1b68bb8c790c
Julian R <56316686+julianmrodri@users.noreply.github.com>
2022-06-20 11:13:35 -0300

    add fail test

--- test/plugins/Asset.test.ts
+++ test/plugins/Asset.test.ts
@@ -48,8 +48,6 @@ describe('Assets contracts #fast', () => {
   const amt = fp('1e4')
 
   before('create fixture loader', async () => {
-    // Reset network for clean execution
-    await hre.network.provider.send('hardhat_reset')
     ;[wallet] = (await ethers.getSigners()) as unknown as Wallet[]
     loadFixture = createFixtureLoader([wallet])
   })
